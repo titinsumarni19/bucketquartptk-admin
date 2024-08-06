22d3c65ba0489d7f39b66cd802feef1b0e72b621
titinsumarni19 <92568394+titinsumarni19@users.noreply.github.com>
2024-08-06 14:15:17 +0700

    middleware

--- app/Http/Middleware/checkLogin.php
+++ app/Http/Middleware/checkLogin.php
@@ -4,17 +4,14 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Session; 
 use Symfony\Component\HttpFoundation\Response;
 
-class checkLogin
+class CheckLogin
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
     {
